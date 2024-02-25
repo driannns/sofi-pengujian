3684c95153ed93741add9ebdd442c2a8a883fc5c
driannns <saputraandrian064@gmail.com>
2024-02-26 00:34:52 +0700

    UI: home and welcome pages

---
+++ src/views/Welcome.jsx
@@ -0,0 +1,55 @@
+import telkomLogo from "../assets/images/telkom.png";
+import Button from "../components/Button";
+import { NavLink } from "react-router-dom";
+
+function Welcome() {
+  return (
+    <div
+      className="d-flex justify-content-center position-relative align-items-center text-center flex-column"
+      style={{
+        backgroundColor: "#fff",
+        fontFamily: "Nunito, sans-serif",
+        fontWeigh: 200,
+        minHeight: "100vh",
+        margin: 0,
+        color: "#636b6f",
+      }}
+    >
+      <img src={telkomLogo} alt="Logo Telkom" height="200px" />
+      <p
+        className="mt-5"
+        style={{
+          fontSize: "84px",
+          fontWeight: 200,
+          fontFamily: "Nunito, sans-serif",
+        }}
+      >
+        SOFI - Sidang Online FRI
+      </p>
+      <div className="mb-4">
+        <a
+          href="#"
+          className="fw-semibold text-decoration-none text-uppercase"
+          style={{
+            color: "#636b6f",
+            padding: "0 25px",
+            fontSize: "13px",
+            letterSpacing: ".1rem",
+          }}
+        >
+          Aplikasi sidang online fakultas rekayasa industri
+        </a>
+      </div>
+      <div className="form-group w-100">
+        {/* <NavLink key="home" to="/home">
+          <Button value="Home" variant="btn-primary" />
+        </NavLink> */}
+        <NavLink key="home" to="/lohin">
+          <Button value="Login" variant="btn-primary" />
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+export default Welcome;
