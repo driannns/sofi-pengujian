379c8abf12a22b90dc0736668a512043b6fa4b75
driannns <saputraandrian064@gmail.com>
2024-02-27 22:43:48 +0700

    Router: delete sidang route

--- src/router.jsx
+++ src/router.jsx
@@ -5,7 +5,6 @@ import Welcome from "./views/Welcome";
 import Home from "./views/Home";
 import Login from "./views/auth/Login";
 import LoginSSO from "./views/auth/LoginSSO";
-import SidangCreate from "./views/sidangs/Create";
 
 const router = (
   <Routes>
@@ -15,7 +14,6 @@ const router = (
 
     <Route element={<PrivateRoutes role="RLMHS" />}>
       <Route path="/home" element={<Home />} />
-      <Route path="/sidangs/create" element={<Home />} />
     </Route>
 
     <Route path="*" element={<p>404 Error - Nothing here...</p>} />
