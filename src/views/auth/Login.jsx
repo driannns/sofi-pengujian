a9e6ef2baaccff0f72fd8ba2bbc24d5831caa8f1
driannns <saputraandrian064@gmail.com>
2024-02-27 22:41:29 +0700

    UI & Function: ui login and dummy function login

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,61 +1,71 @@
 import { GuestLayout } from "../layouts/GuestLayout";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
   return (
     <GuestLayout>
-      <div class="card p-4">
-        <div class="card-body">
-          <form method="post" action="{{ url('/login') }}">
+      <div className="card p-4">
+        <div className="card-body">
+          <form>
             <h1>Login</h1>
-            <p class="text-muted">Sign In to your account</p>
-            <div class="input-group mb-3">
-              <div class="input-group-prepend">
-                <span class="input-group-text">
-                  <i class="icon-user"></i>
+            <p className="text-muted">Sign In to your account</p>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="icon-user"></i>
                 </span>
               </div>
               <input
                 type="username"
-                class="form-control {{ $errors->has('username')?'is-invalid':'' }}"
+                className="form-control {{ $errors->has('username')?'is-invalid':'' }}"
                 name="username"
-                value="{{ old('username') }}"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
                 placeholder="NIM / Kode Dosen"
               />
               {/* @if ($errors->has('username')) */}
-              <span class="invalid-feedback">
+              <span className="invalid-feedback">
                 <strong>NIM / Kode Dosen Anda Salah</strong>
               </span>
               {/* @endif */}
             </div>
-            <div class="input-group mb-4">
-              <div class="input-group-prepend">
-                <span class="input-group-text">
-                  <i class="icon-lock"></i>
+            <div className="input-group mb-4">
+              <div className="input-group-prepend">
+                <span className="input-group-text">
+                  <i className="icon-lock"></i>
                 </span>
               </div>
               <input
                 type="password"
-                class="form-control {{ $errors->has('password')?'is-invalid':'' }}"
+                className="form-control {{ $errors->has('password')?'is-invalid':'' }}"
                 placeholder="Password"
                 name="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
               />
               {/* @if ($errors->has('password')) */}
-              <span class="invalid-feedback">
+              <span className="invalid-feedback">
                 <strong>Password Anda Salah</strong>
               </span>
               {/* @endif */}
             </div>
-            <div class="row">
-              <div class="col-6">
-                <button class="btn btn-primary px-4" type="submit">
+            <div className="row">
+              <div className="col-6">
+                <button className="btn btn-primary px-4" type="submit">
                   Login
                 </button>
               </div>
-              <div class="col-6 text-right">
+              <div className="col-6 text-right">
                 <NavLink
-                  key="loginSSO"
-                  to="/loginSSO"
+                  key="loginsso"
+                  to="/loginsso"
                   className="btn btn-link px-0"
                 >
                   Login via SSO Igracias
@@ -66,16 +76,16 @@ const Login = () => {
         </div>
       </div>
       <div
-        class="card text-white bg-primary py-5 d-md-down-none"
+        className="card text-white bg-primary py-5 d-md-down-none"
         style={{ width: "44%" }}
       >
-        <div class="card-body text-center">
+        <div className="card-body text-center">
           <div>
             <h2>Sign up</h2>
             <p>belum memiliki akun ? silahkan menghubungi admin akademik FRI</p>
             Kontak LAAK FRI :{" "}
             <a
-              class="btn btn-primary active mt-3"
+              className="btn btn-primary active mt-3"
               href="<?php echo 'http://wa.me/'.$no_laa ?>"
             >
               "(?php echo $no_laa)"
