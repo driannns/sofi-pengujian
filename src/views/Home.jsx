be4b296fca77f848bed1ee23cfb22ae049bf3642
driannns <saputraandrian064@gmail.com>
2024-03-06 15:03:17 +0700

    UI: add alert function and add middleware

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,86 +1,110 @@
 import { MainLayout } from "./layouts/MainLayout";
 import { jwtDecode } from "jwt-decode";
 import { useCookies } from "react-cookie";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Home = () => {
   const [cookies] = useCookies();
   const authToken = cookies["auth-token"];
   const userData = jwtDecode(authToken);
+  const statusSidang = null;
+  const kaurAkademik = "Edi Sutoyo, S.Kom., M.CompSc.";
+  const no_laa = "+6281311997199";
   const location = useLocation();
-  console.log(location.state?.error);
+  const [error, setError] = useState(null);
+
+  const clearLocalStorage = () => {
+    localStorage.removeItem("error");
+  };
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      if (error) {
+        localStorage.setItem("error", error);
+      } else {
+        clearLocalStorage();
+      }
+    };
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [error]);
+
+  useEffect(() => {
+    clearLocalStorage();
+  }, []);
+
   return (
     <MainLayout>
       <div className="container-fluid">
         <div className="animated fadeIn">
           <div className="row mt-3">
-            {/* <div className="col-12">@include('flash::message')</div> */}
             <div className="col-12">
-              {location.state?.error && (
-                <div class="alert alert-danger" role="alert">
-                  {location.state?.error}
+              {localStorage.getItem("message") && (
+                <div className="alert alert-danger" role="alert">
+                  {localStorage.getItem("message")}
                 </div>
               )}
             </div>
           </div>
           <div className="row">
-            {userData.role?.find((roles) => "RLADM".includes(roles)) && (
+            {userData.role?.find((roles) => "RLADM".includes(roles)) ? (
               <div className="card">
                 <div className="card-header">
-                  <h1>Selamat Datang username(admin)</h1>
+                  <h1>Selamat Datang username {userData?.nama}</h1>
                 </div>
                 <div className="card-body">
-                  <p>
-                    <ol>
-                      <li>
-                        Pastikan parameter pada id <b>academicPeriod</b> sudah
-                        sesuai dengan periode sidang sekarang
-                      </li>
-                      <li>Update dahulu data dosen di menu data dosen</li>
-                      <li>Isi Data Period</li>
-                      <li>Isi Data CLO di menu Setting CLO</li>
-                      <li>
-                        Jika ada user dosen yang ingin di set menjadi role
-                        tertentu silahkan masuk ke menu Manage Role
-                      </li>
-                    </ol>
-                  </p>
+                  <ol>
+                    <li>
+                      Pastikan parameter pada id <b>academicPeriod</b> sudah
+                      sesuai dengan periode sidang sekarang
+                    </li>
+                    <li>Update dahulu data dosen di menu data dosen</li>
+                    <li>Isi Data Period</li>
+                    <li>Isi Data CLO di menu Setting CLO</li>
+                    <li>
+                      Jika ada user dosen yang ingin di set menjadi role
+                      tertentu silahkan masuk ke menu Manage Role
+                    </li>
+                  </ol>
                 </div>
               </div>
-            )}
-
-            {/* else */}
-            <div className="col-12">
-              <h3>TATA TERTIB PELAKSAAN SIDANG TUGAS AKHIR</h3>
-              {/* <!-- update notif lulus --> */}
-              {/* @if(Auth::user()->isStudent())
-                    @if($statussidang)
-                        @if($statussidang->status == 'lulus')
-                            <div className="alert alert-success" role="alert">
-                                Selamat Anda Dinyatakan <b>LULUS</b> pada sidang periode {{$statussidang->period_id}}
-                            </div>
-                        @endif
-                    @endif
-                @endif */}
-              <hr className="mt-0" />
-              <h6 className="mb-3">
-                <a href="{{ route('home') }}" className="text-dark">
-                  BERANDA
-                </a>
-              </h6>
-              <div className="card">
-                <div className="card-body">
-                  <p>
-                    Jika sudah mendapatkan jadwal sidang Mohon konfirmasi ke
-                    dosen pembimbing dan penguji
-                  </p>
+            ) : (
+              <div className="col-12">
+                <h3>TATA TERTIB PELAKSAAN SIDANG TUGAS AKHIR</h3>
+                {/* <!-- update notif lulus --> */}
+                {userData.role?.find((roles) => "RLMHS".includes(roles)) &&
+                  // @if($statussidang)
+                  statusSidang &&
+                  statusSidang.status === "lulus" && (
+                    <div className="alert alert-success" role="alert">
+                      Selamat Anda Dinyatakan <b>LULUS</b> pada sidang periode{" "}
+                      {statusSidang.period_id}
+                    </div>
+                  )}
+                <hr className="mt-0" />
+                <h6 className="mb-3">
+                  <Link to="/home" className="text-dark">
+                    BERANDA
+                  </Link>
+                </h6>
+                <div className="card">
+                  <div className="card-body">
+                    <p>
+                      Jika sudah mendapatkan jadwal sidang Mohon konfirmasi ke
+                      dosen pembimbing dan penguji
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="card">
-                <div className="card-body">
-                  <p>
-                    Berikut adalah tata tertib yang harus dipenuhi selama sidang
-                    tugas akhir berlangsung:
+                <div className="card">
+                  <div className="card-body">
+                    <p>
+                      Berikut adalah tata tertib yang harus dipenuhi selama
+                      sidang tugas akhir berlangsung:
+                    </p>
                     <ol>
                       <li>
                         Peserta sidang berpakaian rapi dan Menggunakan jas
@@ -118,19 +142,17 @@ const Home = () => {
                     Ka.Urusan Akademik
                     <br></br>
                     <br></br>
-                    <span className="font-weight-bold">
-                      php echo $kaur_akademik
-                    </span>
-                  </p>
-                  <p>
-                    Kontak LAAK FRI :{" "}
-                    <a href="<?php echo 'http://wa.me/'.$no_laa ?>">
-                      php echo $no_laa
-                    </a>
-                  </p>
+                    <span className="font-weight-bold">{kaurAkademik}</span>
+                    <p>
+                      Kontak LAAK FRI :{" "}
+                      <a target="_blank" href={`http://wa.me/${no_laa}`}>
+                        {no_laa}
+                      </a>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
           <div className="row">
             {userData.role?.find((roles) => "RLDSN".includes(roles)) && (
@@ -151,8 +173,8 @@ const Home = () => {
                           </tr>
                         </thead>
                         <tbody>
-                          @foreach($schedules as $data) @foreach( $data as
-                          $schedule)
+                          {/* @foreach($schedules as $data) @foreach( $data as
+                          $schedule) */}
                           <tr>
                             <td> $schedule-sidang-mahasiswa-nim</td>
                             <td> $schedule-sidang-mahasiswa-user-nama</td>
@@ -165,7 +187,7 @@ const Home = () => {
                               </a>
                             </td>
                           </tr>
-                          @endforeach @endforeach
+                          {/* @endforeach @endforeach */}
                         </tbody>
                       </table>
                     </div>
@@ -187,8 +209,8 @@ const Home = () => {
                           </tr>
                         </thead>
                         <tbody>
-                          @foreach($revisions as $data) @foreach( $data as
-                          $revision)
+                          {/* @foreach($revisions as $data) @foreach( $data as
+                          $revision) */}
                           <tr>
                             <td>$revision-sidang-mahasiswa-nim</td>
                             <td>$revision-sidang-mahasiswa-user-nama</td>
@@ -201,7 +223,7 @@ const Home = () => {
                               </a>
                             </td>
                           </tr>
-                          @endforeach @endforeach
+                          {/* @endforeach @endforeach */}
                         </tbody>
                       </table>
                     </div>
@@ -223,8 +245,8 @@ const Home = () => {
                           </tr>
                         </thead>
                         <tbody>
-                          @foreach($schedulesNotComplete as $data) @foreach(
-                          $data as $schedule)
+                          {/* @foreach($schedulesNotComplete as $data) @foreach(
+                          $data as $schedule) */}
                           <tr>
                             <td>$schedule-sidang-mahasiswa-nim</td>
                             <td>$schedule-sidang-mahasiswa-user-nama</td>
@@ -237,7 +259,7 @@ const Home = () => {
                               </a>
                             </td>
                           </tr>
-                          @endforeach @endforeach
+                          {/* @endforeach @endforeach */}
                         </tbody>
                       </table>
                     </div>
