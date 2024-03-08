2101cf5af213250a24368db0cc2a4970014d2191
driannns <saputraandrian064@gmail.com>
2024-03-08 16:37:38 +0700

    Sidang Redux
    Add sidang data to redux with checkSidang action and the others

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
 import { lecturerReducer } from "./modules/lecturer/reducer";
+import { sidangReducer } from "./modules/sidang/reducer";
 
 const rootReducer = combineReducers({
   lecturer: lecturerReducer,
+  sidang: sidangReducer,
 });
 const middleware = [thunk];
 
