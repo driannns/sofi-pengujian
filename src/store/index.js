d14658f853c5b3f3ceb4da3e39fe44b7cfd4267a
driannns <saputraandrian064@gmail.com>
2024-03-13 23:55:50 +0700

    Dependencies: add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,31 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
 import { lecturerReducer } from "./modules/lecturer/reducer";
 import { sidangReducer } from "./modules/sidang/reducer";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 const rootReducer = combineReducers({
   lecturer: lecturerReducer,
   sidang: sidangReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleware = [thunk];
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(middleware),
 });
 
+const persistor = persistStore(store);
+
 export default store;
+export { persistor };
