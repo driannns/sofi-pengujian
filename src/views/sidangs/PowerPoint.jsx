3295b494603ed5e9b276a633c618a0b53a78540a
driannns <saputraandrian064@gmail.com>
2024-03-28 15:38:27 +0700

    UI: Func: slides function and state management

--- src/views/sidangs/PowerPoint.jsx
+++ src/views/sidangs/PowerPoint.jsx
@@ -1,19 +1,23 @@
 import { MainLayout } from "../layouts/MainLayout";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { checkSidang } from "../../store/sidangSlicer";
 import { useCookies } from "react-cookie";
-const APISOFI = "https://ca07-182-2-46-163.ngrok-free.app/api";
+import Alert from "../../components/Alert";
+
+const APISOFI = "https://6f73-180-253-71-196.ngrok-free.app";
 
 const MateriPresentasi = () => {
-  const { data: dataSidang } = useSelector((state) => state.sidang);
+  const dataSidang = useSelector((state) => state.sidang);
 
-  const location = useLocation();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [cookies] = useCookies();
-  const [docTA, setDocTA] = useState();
+
+  const [periodNow, setPeriodNow] = useState(null);
+  const [oldPeriod, setOldPeriod] = useState(null);
+  const [slide, setSlide] = useState(null);
   const [makalah, setMakalah] = useState();
 
   const handleDocTAChange = (e) => {
@@ -27,17 +31,69 @@ const MateriPresentasi = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await dispatch(checkSidang(cookies["auth-token"]));
-        const res = await axios.get(`${APISOFI}/slide/get-latest-slide`, {
+        const dataSidangStudent = await dispatch(
+          checkSidang(cookies["auth-token"])
+        );
+
+        if (!dataSidangStudent.payload) {
+          localStorage.setItem("errorMessae", "Anda belum mendaftar sidang!");
+          navigate(-1);
+        }
+
+        if (
+          dataSidangStudent.payload.status === "sudah dijadwalkan" ||
+          dataSidangStudent.payload.status === "tidak lulus (sudah dijadwalkan)"
+        ) {
+          localStorage.setItem(
+            "warningMessage",
+            "Sidang anda sudah dijadwalkan, tidak dapat merubah file presentasi"
+          );
+          navigate("/schedule/mahasiswa"); //?Belum dibuat
+        }
+
+        if (
+          !dataSidangStudent.payload.status.includes([
+            "telah disetujui admin",
+            "belum dijadwalkan",
+            "tidak lulus",
+            "tidak lulus (sudah update dokumen)",
+            "tidak lulus (belum dijadwalkan)",
+          ])
+        ) {
+          localStorage.setItem(
+            "errorMessage",
+            "Sidang anda belum di approve dosen pembimbing dan admin"
+          );
+          navigate(-1);
+        }
+
+        const res = await axios.get(`${APISOFI}/api/slide/get-latest-slide`, {
           headers: {
             Authorization: `Bearer ${cookies["auth-token"]}`,
             "ngrok-skip-warning-browser": true,
           },
         });
+
+        console.log(res);
+        setSlide(res.data);
+
+        if (dataSidangStudent.payload.status === "tidak lulus") {
+          setOldPeriod(dataSidangStudent.payload.period_id);
+          const resPeriodNow = await axios.get(
+            `${APISOFI}/api/period/get/${dataSidangStudent.payload.period_id}`,
+            {
+              headers: {
+                Authorization: `Bearer ${cookies["auth-token"]}`,
+                "ngrok-skip-browser-warning": true,
+              },
+            }
+          );
+          setPeriodNow(resPeriodNow.data);
+        }
       } catch (err) {
-        console.error(err);
         localStorage.setItem("errorMessage", "Network error");
-        navigate("/home");
+        // navigate("/home");
+        navigate(-1);
       }
     };
 
@@ -94,12 +150,7 @@ const MateriPresentasi = () => {
       <div className="container-fluid">
         <div className="animated fadeIn">
           {/*@include('flash::message') @include('coreui-templates::common.errors')*/}
-          {location.state?.error && (
-            <div className="alert alert-danger" role="alert">
-              {location.state.error}
-            </div>
-          )}
-
+          <Alert type="danger" />
           {dataSidang.data &&
             (dataSidang.data.status === "tidak lulus" ||
               dataSidang.data.status === "titak lulus (sudah update dokumen)" ||
@@ -108,12 +159,7 @@ const MateriPresentasi = () => {
                 <div className="col-lg-12">
                   <div className="card">
                     <div className="card-body">
-                      <form
-                        className=""
-                        // action="{{ route('sidang-ulang.update', $sidang->id) }}"
-                        // method="post"
-                        // enctype="multipart/form-data"
-                      >
+                      <form>
                         {/* @csrf */}
                         <input
                           type="text"
@@ -127,9 +173,22 @@ const MateriPresentasi = () => {
                             revisi
                           </p>
                           <div className="form-group">
-                            <label htmlFor="">Periode Sidang Ulang</label>
-                            {/*{!! Form::select('period_id', $periods, $oldPeriod, ['className' => 'select2
-                                    form-control'])!!}*/}
+                            <label htmlFor="period_id">
+                              Periode Sidang Ulang
+                            </label>
+                            <select
+                              name="period_id"
+                              id="period_id"
+                              className="select2 form-control"
+                              value={oldPeriod && oldPeriod}
+                            >
+                              {periodNow &&
+                                periodNow.map((value, index) => (
+                                  <option key={index} value={value.id}>
+                                    {value.name}
+                                  </option>
+                                ))}
+                            </select>
                           </div>
                           <div className="form-group">
                             <label htmlFor="">Dokumen TA Sidang Ulang</label>
@@ -137,7 +196,7 @@ const MateriPresentasi = () => {
                               <input
                                 type="file"
                                 className="custom-file-input forminput"
-                                name="dokumen_ta"
+                                name="doc_ta"
                                 onChange={handleDocTAChange}
                               />
                               <label className="custom-file-label">
@@ -189,13 +248,7 @@ const MateriPresentasi = () => {
                     </div>
 
                     <div className="col">
-                      <form
-                        className=""
-                        // action="{{ route('slides.upload') }}"
-                        // method="post"
-                        // enctype="multipart/form-data"
-                      >
-                        {/* @csrf */}
+                      <form>
                         <input
                           type="text"
                           name="sidang_id"
@@ -204,7 +257,7 @@ const MateriPresentasi = () => {
                         />
                         <div className="input-group mb-3">
                           <div className="custom-file">
-                            {true ? (
+                            {slide ? (
                               <>
                                 <input
                                   type="file"
@@ -212,7 +265,7 @@ const MateriPresentasi = () => {
                                   name="slide"
                                 />
                                 <label className="custom-file-label">
-                                  {/* {slide.url} */}
+                                  {slide.url}
                                 </label>
                               </>
                             ) : (
@@ -230,10 +283,10 @@ const MateriPresentasi = () => {
                             )}
                           </div>
                         </div>
-                        {true ? (
+                        {slide ? (
                           <div className="row ml-0">
                             <a
-                              href="/{{ $slide->file_url }}"
+                              href={`${APISOFI}/public/slides/${slide.url}`}
                               className="btn btn-danger mr-2"
                             >
                               Download
