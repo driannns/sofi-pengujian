6f60024e97c7e7081d3175d3f341aaddc055f1b8
driannns <saputraandrian064@gmail.com>
2024-06-18 15:28:47 +0700

    test

--- src/views/layouts/GuestLayout.jsx
+++ src/views/layouts/GuestLayout.jsx
@@ -2,6 +2,11 @@ export function GuestLayout(props) {
   return (
     <div className="app flex-row align-items-center">
       <div className="container">
+        <div className="sidebar shadow" style={{ display: "none" }}>
+          <nav className="sidebar-nav">
+            <ul className="nav"></ul>
+          </nav>
+        </div>
         <div className="row justify-content-center">
           <div className="col-md-8">
             <div className="card-group">{props.children}</div>
