f7d2a9361063b661a98af148901fb0710c504077
driannns <saputraandrian064@gmail.com>
2024-06-26 10:11:53 +0700

    fix home dosen

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -171,23 +171,7 @@ const Home = () => {
                                 <th>Action</th>
                               </tr>
                             </thead>
-                            <tbody>
-                              {/* @foreach($schedules as $data) @foreach( $data as
-                          $schedule) */}
-                              <tr>
-                                <td> $schedule-sidang-mahasiswa-nim</td>
-                                <td> $schedule-sidang-mahasiswa-user-nama</td>
-                                <td>
-                                  <a
-                                    href="{{ route('scores.pembimbing.create', [$schedule->id]) }}"
-                                    className="btn btn-primary w-100"
-                                  >
-                                    Nilai
-                                  </a>
-                                </td>
-                              </tr>
-                              {/* @endforeach @endforeach */}
-                            </tbody>
+                            <tbody></tbody>
                           </table>
                         </div>
                       </div>
@@ -207,23 +191,7 @@ const Home = () => {
                                 <th>Action</th>
                               </tr>
                             </thead>
-                            <tbody>
-                              {/* @foreach($revisions as $data) @foreach( $data as
-                          $revision) */}
-                              <tr>
-                                <td>$revision-sidang-mahasiswa-nim</td>
-                                <td>$revision-sidang-mahasiswa-user-nama</td>
-                                <td>
-                                  <a
-                                    href="{{ route('revisions.index.dosen') }}"
-                                    className="btn btn-warning w-100"
-                                  >
-                                    Revisi
-                                  </a>
-                                </td>
-                              </tr>
-                              {/* @endforeach @endforeach */}
-                            </tbody>
+                            <tbody></tbody>
                           </table>
                         </div>
                       </div>
@@ -243,23 +211,7 @@ const Home = () => {
                                 <th>Action</th>
                               </tr>
                             </thead>
-                            <tbody>
-                              {/* @foreach($schedulesNotComplete as $data) @foreach(
-                          $data as $schedule) */}
-                              <tr>
-                                <td>$schedule-sidang-mahasiswa-nim</td>
-                                <td>$schedule-sidang-mahasiswa-user-nama</td>
-                                <td>
-                                  <a
-                                    href="{{ route('scores.simpulan', [$schedule->id]) }}"
-                                    className="btn btn-danger w-100"
-                                  >
-                                    Simpulan
-                                  </a>
-                                </td>
-                              </tr>
-                              {/* @endforeach @endforeach */}
-                            </tbody>
+                            <tbody></tbody>
                           </table>
                         </div>
                       </div>
